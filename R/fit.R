73c32a0ed59ac6dc0e2d9c368f6ca643a02c1660
Markus Ulmer <markus.ulmer@stat.math.ethz.ch>
2024-10-29 12:41:44 +0100

    fit ready for euler

--- R/fit.R
+++ R/fit.R
@@ -1,32 +1,42 @@
-library(ranger)
+#library(ranger)
 library(SDForest)
 source("R/utils.R")
 
 dat <- prepAggData()
-X <- dat$X
+X <- as.matrix(dat$X)
 Y <- dat$Y
 A <- dat$A
 p_names <- dat$p_names
 perturbations <- dat$perturbations
 pertLabel <- dat$pertLabel
 
+#hist(apply(X, 2, function(x)length(unique(x))), breaks = 1000)
+
+X <- X[, apply(X, 2, function(x)length(unique(x))) >= 200]
+n <- length(Y)
 
 anchor_dose <- sapply(strsplit(perturbations, ' '), function(x) x[1])
 Groups <- unique(anchor_dose)
 
-Groups
-anchor_dose
-perturbations
-unique(pertLabel)
-A
-dim(X)
+envs <- rep(0, n)
+
+for(group in Groups){
+  test_pert <- perturbations[anchor_dose == group]
+  envs[which(pertLabel %in% test_pert)] <- group
+}
+envs <- as.factor(envs)
 
 start <- Sys.time()
-fit_sdf <- SDForest(x = X, y = Y, nTree = 100, return_data = F, multicore = F, 
-                    mem_size = 4e+7, leave_out_ind = test_env, max_size = 100, 
-                    envs = )
+fit_sdf <- SDForest(x = X, y = Y, envs = envs, nTree_leave_out = 50, mc.cores = 100)
+fit_sdf <- toList(fit_sdf)
 end <- Sys.time()
-print(paste0("Training time: ", end - start))
+print(paste0("Training time SDF: ", end - start))
 
-print(paste0("MSE: ", fit_sdf$oob_loss))
-print(paste0("SDE: ", fit_sdf$oob_SDloss))
+start <- Sys.time()
+fit_plain <- SDForest(x = X, y = Y, envs = envs, nTree_leave_out = 50, mc.cores = 100,
+                    Q_type = "no_deconfounding")
+fit_plain <- toList(fit_plain)
+end <- Sys.time()
+print(paste0("Training time PLAIN: ", end - start))
+
+save(fit_sdf, fit_plain, fitle = "results/fits.Rdata")
