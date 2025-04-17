1033c62672c6439ee94882d89cfb94b3e1d4c302
Markus Ulmer <markus.ulmer@stat.math.ethz.ch>
2025-04-17 16:03:50 +0200

    hdi model

--- R/drugInteraction.R
+++ R/drugInteraction.R
@@ -14,13 +14,34 @@ dat0 <- dat0[data$protein_plate, -1]
 dat <- data
 dat <- cbind(dat, dat0)
 
+# remove M453(ATCC) (no baseline protein expression)
+dat <- dat[dat$protein_plate != "M453(ATCC)", ]
+
+# add nois to imputation for computational und statistical stability
+add_noise_to_imputation <- function(x){
+  border <- sort(unique(x))[2]
+  imp <- which(x < border)
+  x_ <- x[-imp]
+  y <- x
+  y[imp] <- truncnorm::rtruncnorm(length(imp), a = -Inf, b = border, mean = mean(x_), sd = sd(x_))
+  y
+}
+
+datI <- dat
+plate_idx <- lapply(unique(dat$protein_plate), function(plate) which(dat$protein_plate == plate))
+
+for(p in prot_names){
+  for(plate in plate_idx)
+    datI[plate, p] <- add_noise_to_imputation(datI[plate, p])
+}
+
 # protein of interest
 P <- "P08621.P08621.RU17_HUMAN.SNRNP70.U1.small.nuclear.ribonucleoprotein.70.kDa"
 t <- 6
 
 # Data for model
-Y <- dat[dat$pert_time == t, P] - dat[dat$pert_time == t, paste0(P, "_0")]
-D <- dat[dat$pert_time == t, pert_names]
+Y <- datI[datI$pert_time == t, P] - datI[datI$pert_time == t, paste0(P, "_0")]
+D <- datI[datI$pert_time == t, pert_names]
 modelDat <- data.frame(Y = Y, D = D)
 
 fit1 <- lm(Y ~ -1 + .^2, modelDat)
@@ -28,6 +49,48 @@ col <- c(rep("blue", ncol(D)), rep("red", choose(ncol(D), 2)))
 plot(fit1$coefficients, col = col)
 
 # debiased lasso
+library(hdi)
+
+## prepare design matrix with interactions
+drug_design <- model.matrix(~ -1 + .^2, data = D)
+
+# remove tratements wihtout data
+with_data <- apply(drug_design, 2, function(x) length(unique(x))) != 1
+drug_design <- drug_design[, with_data]
+
+# add intercept to each treatment
+drug_intercept <- drug_design
+drug_intercept[drug_design != 0] <- 1
+colnames(drug_intercept) <- paste0(colnames(drug_design), "_intercept")
+
+# combine intercept and drug design
+drug_design <- cbind(drug_design, drug_intercept)
+
+#labels
+single_effects <- rep(c(colnames(D), rep(NA, choose(ncol(D), 2))), 2)
+length(single_effects[c(with_data, with_data)])
+dim(drug_design)
+
+fit2 <- lasso.proj(x = drug_design, y = Y, ncores = 11, parallel = TRUE)
+plot(fit2$pval)
+plot(fit2$pval.corr)
+
+fit2
+
+dev.off()
+svd(D)
+which(is.na(dat[dat$pert_time == t, paste0(P, "_0")]))
+plot(drug_design[, '`drug_#56`:`drug_#64`'])
+unique(dat$protein_plate)
+
+dat[dat$pert_time == t, ][which(is.na(dat[dat$pert_time == t, paste0(P, "_0")])), "protein_plate"]
+apply(D, 2, function(x) cor(x, Y))
+
+
+plot(Y)
+dev.off()
+
+
 library(pemultinom)
 install.packages("pemultinom")
 
@@ -45,6 +108,9 @@ plot(D[1, ])
 plot(colMeans(D))
 
 library(hdi)
+lasso.proj(x = D, y = Y)
+
+
 
 Y <- jitter(Y)
 Y <- rnorm(length(Y))
@@ -59,17 +125,14 @@ fit2 <- hdi(x = D, y = Y)
 library(truncnorm)
 ?rtruncnorm
 
-add_noise_to_imputation <- function(x){
-    x <- dat[dat$protein_plate == dat$protein_plate[1], P]
-    border <- sort(unique(x))[2]
-    imp <- which(x < border)
-    x_ <- x[-imp]
-    y <- x
-    y[imp] <- truncnorm::rtruncnorm(length(imp), a = -Inf, b = border, mean = mean(x_), sd = sd(x_))
-    Y
-}
+x <- dat[dat$protein_plate == dat$protein_plate[1], P]
 
-plot(x)
-points(y, col = 3)
 
-plot(x, y)
+
+plot(dat[, 10], datI[, 10], col = as.factor(dat$protein_plate))
+
+dat[, p]
+y <- tapply(dat[, p], dat$protein_plate, add_noise_to_imputation)
+dim(y)
+length(y)
+plot(dat[, p], y)
