c001f04619f21bc32cbd12018e46f9de71ab89cc
markus <markus_ulmer@bluewin.ch>
2025-05-06 10:26:20 +0200

    add aggregation

--- R/drugInteraction.R
+++ R/drugInteraction.R
@@ -1,9 +1,17 @@
+library(hdi)
+library(plotly)
+
 load("data/prepData.RData")
 load("data/protNames.RData")
+dim(data)
 
 #mean per protein plate at time 0
 dat0 <- data[data$pert_time == 0, ]
 dat0 <- aggregate(dat0[, prot_names], by = list(dat0$protein_plate), FUN = mean)
+
+# sort drugs by information
+drugOrder <- names(sort(colSums(data[, pert_names] != 0), decreasing = T))
+drugOrder <- paste0('`', drugOrder, '`')
 
 # add mean at 0 to data
 names(dat0)[-1] <- paste0(names(dat0)[-1], "_0")
@@ -35,16 +43,41 @@ for(p in prot_names){
     datI[plate, p] <- add_noise_to_imputation(datI[plate, p])
 }
 
+### collect aggregated lagged times
+# unique experiments
+uniqueExp <- unique(datI[!(datI$pertLabel %in% c('no', 'no no')), 
+                         c("Anchor_dose", "Library_dose", 
+                           "pertLabel", 'protein_plate')])
+str(uniqueExp)
+expTimes <- c(6, 24, 48)
+prev <- 6
+
+experiment <- uniqueExp[1, ]
+
+aggData <- sapply(1:nrow(uniqueExp), function(i){
+  experiment <- uniqueExp[i, ]
+  
+  anchor <- datI$Anchor_dose == experiment$Anchor_dose
+  libr <- datI$Library_dose == experiment$Library_dose
+  pert <- datI$pertLabel == experiment$pertLabel
+  plate <- datI$protein_plate == experiment$protein_plate
+  tim <- datI$pert_time == prev
+
+  sel <- anchor & libr & pert & plate & tim
+  
+  if(sum(sel) == 0) print(experiment)
+  colMeans(data[sel, prot_names])
+})
+
+aggData[1:3, 1680]
+
 # protein of interest
 P <- "P08621.P08621.RU17_HUMAN.SNRNP70.U1.small.nuclear.ribonucleoprotein.70.kDa"
-t <- 6
+t <- 48
 
 # Data for model
 Y <- datI[datI$pert_time == t, P] - datI[datI$pert_time == t, paste0(P, "_0")]
 D <- datI[datI$pert_time == t, pert_names]
-
-# debiased lasso
-library(hdi)
 
 ## prepare design matrix with interactions
 drug_design <- model.matrix(~ -1 + .^2, data = D)
@@ -63,13 +96,21 @@ colnames(drug_intercept) <- paste0(colnames(drug_design), "_intercept")
 
 # combine intercept and drug design
 drug_design <- cbind(drug_design, drug_intercept)
+design <- drug_design
+
+# for time 24 and 48 use lagged protein expression
+
+datI[datI$pert_time == t, ]
+
+design <- cbind(design, protein_design)
+
 
 #labels
 single_effects <- rep(c(colnames(D), rep(NA, choose(ncol(D), 2))), 2)
 colnames(drug_design)
 
 #hdi fit
-fit <- lasso.proj(x = drug_design, y = Y, ncores = 11, parallel = TRUE)
+fit <- lasso.proj(x = design, y = Y, ncores = 11, parallel = TRUE)
 
 pMat <- matrix(NA, nrow = ncol(D), ncol = ncol(D))
 rownames(pMat) <- colnames(pMat) <- dLabels[1:ncol(D)]
@@ -85,15 +126,11 @@ for(l in dLabels_measured){
 pMat
 pMat[is.na(pMat)] <- 2
 
-res <- heatmap(pMat)
-
-pMat <- pMat[res$rowInd, res$colInd]
+pMat <- pMat[drugOrder, drugOrder]
 #pMat <- -log(pMat)
 pMat <- round(pMat, 3)
 
-library(plotly)
-  
+
 ht <- plot_ly(z = pMat, x = colnames(pMat), y = colnames(pMat), 
                 type = "heatmap", colors = "Greys")
 ht
-htmlwidgets::saveWidget(as_widget(ht), "p6c.html")
