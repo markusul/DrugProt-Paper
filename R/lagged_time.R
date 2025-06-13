a29d2bd3dcf1b16684e855433055dd6cb4cede8c
markus <markus_ulmer@bluewin.ch>
2025-06-13 17:01:14 +0200

    preparing lagged data

--- R/lagged_time.R
+++ R/lagged_time.R
@@ -1,7 +1,5 @@
-library(hdi)
-
-load("data/laggedData.RData")
-expTimes <- c(6, 24, 48)
+load("data/prepData.RData")
+load("data/protNames.RData")
 
 data$pertLabel[data$pertLabel == 'no no'] <- 'no'
 
@@ -77,7 +75,6 @@ aggData <- lapply(expTimes, function(prev) {
   }))
 })
 
-
 # add experiment label
 datInfo <- datI[, c("Anchor_dose", "Library_dose", "pertLabel", 'protein_plate')]
 label <- rep(NA, nrow(datInfo))
@@ -94,65 +91,4 @@ for(i in 1:nrow(uniqueExp)){
 }
 datI$label <- label
 
-# protein of interest
-#P <- "P08621.P08621.RU17_HUMAN.SNRNP70.U1.small.nuclear.ribonucleoprotein.70.kDa"
-P <- prot_names[1]
-
-t <- 6
-for(t in expTimes){
-  print(P)
-  print(t)
-  
-  # Data for model
-  Y <- datI[datI$pert_time == t, P] - datI[datI$pert_time == t, paste0(P, "_0")]
-  D <- datI[datI$pert_time == t, pert_names]
-  
-  ## prepare design matrix with interactions
-  drug_design <- model.matrix(~ -1 + .^2, data = D)
-  dLabels <- c(colnames(drug_design), colnames(drug_design))
-  
-  # remove tratements wihtout data
-  with_data <- apply(drug_design, 2, function(x) length(unique(x))) != 1
-  drug_design <- drug_design[, with_data]
-  dLabels_measured <- colnames(drug_design)
-  dlabels_model <- c(dLabels_measured, dLabels_measured)
-  
-  # add intercept to each treatment
-  drug_intercept <- drug_design
-  drug_intercept[drug_design != 0] <- 1
-  colnames(drug_intercept) <- paste0(colnames(drug_design), "_intercept")
-  
-  # combine intercept and drug design
-  drug_design <- cbind(drug_design, drug_intercept)
-  design <- drug_design
-  
-  #protein design
-  laggedTime <- which(expTimes == t) - 1
-  
-  if(laggedTime > 0){
-    max(datI[datI$pert_time == t, 'label'])
-    sum(is.na(aggData[[laggedTime]]))
-    sum(rowSums(is.na(aggData[[laggedTime]])) > 0)
-    protein_design <- aggData[[laggedTime]][datI[datI$pert_time == t, 'label'], ]
-    
-    #design <- cbind(design, protein_design)
-    design <- protein_design
-    
-    # remove samples without lagged protein measurements
-    noLagged <- rowSums(is.na(design)) > 0
-    design <- design[!noLagged, ]
-    Y <- Y[!noLagged]
-  }
-  
-  #labels
-  single_effects <- rep(c(colnames(D), rep(NA, choose(ncol(D), 2))), 2)
-  colnames(drug_design)
-  
-  #hdi fit
-  fit <- lasso.proj(x = design, y = Y, return.Z = T, suppress.grouptesting = t>6, parallel = T, ncores = 20)
-  #save Z
-  Z <- fit$Z
-  save(file = paste0('Z/', t, '.RData'), Z)
-}
-
-print("finished!")
+save(datI, prot_names, pert_names, aggData, file = 'data/laggedData.RData')
