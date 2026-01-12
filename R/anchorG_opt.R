c78758d7ac648e71ef96bb6d685ae4b837ae6405
Markus Ulmer <maulmer@student.ethz.ch>
2026-01-12 13:23:16 +0100

    new sdmodels version

--- R/anchorG_opt.R
+++ R/anchorG_opt.R
@@ -19,31 +19,15 @@ pertLabel <- dat$pertLabel
 X <- X[, apply(X, 2, function(x)length(unique(x))) >= 200]
 n <- length(Y)
 
-# define 8 drug combination groups based on equal anchor drug
-anchor_dose <- sapply(strsplit(perturbations, ' '), function(x) x[1])
-Groups <- unique(anchor_dose)
-
-envs <- rep(0, n)
-for(group in Groups){
-  test_pert <- perturbations[anchor_dose == group]
-  envs[which(pertLabel %in% test_pert)] <- group
-}
-envs <- as.factor(envs)
-
-# fit 200 trees for each environment left out
-trees_envs <- rep(200, length(levels(envs)))
-names(trees_envs) <- levels(envs)
-trees_envs['0'] <- 0
-
 # optimal gamma from cross-validation
-gamma <- 3.443544
+gamma <- 3.448345
+#3.443544
 
 # fit Anchor Forest
-fit_anchor <- SDForest(x = X, y = Y, A = A, nTree = 10,
+options(future.globals.maxSize = 2.0 * 1e9) # increase max global size for parallel processing
+fit_anchor <- SDForest(x = X, y = Y, A = A, nTree = 1000,
                        Q_type = "no_deconfounding", gamma = gamma, 
-                       cp = 0, mc.cores = 20, gpu = F)
-# save fitted model as list
-fit_anchor <- toList(fit_anchor)
+                       cp = 0, mc.cores = 100)
 save(fit_anchor, gamma, file = paste0("results/anchorG_opt.RData"))
 
 end_time <- Sys.time()
